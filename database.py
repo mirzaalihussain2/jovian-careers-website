29c35b8fc15895a6f12aedab9432781cf9a9133b
ali hussain <ali@gizmo.ai>
2024-11-07 22:54:47 +0000

    Successfully inserting into applications table

--- database.py
+++ database.py
@@ -33,11 +33,12 @@ def add_application_to_db(job_id, application):
     with engine.connect() as conn:
         query = text('insert into applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)')
         conn.execute(query, {
-                     "job_id": job_id,
-                     "full_name": application['full_name'],
-                     "email": application['email'],
-                     "linkedin_url": application['linkedin_url'],
-                     "education": application['education'],
-                     "work_experience": application['work_experience'],
-                     "resume_url": application['resume_url']}
-        )
+            "job_id": job_id,
+            "full_name": application['full_name'],
+            "email": application['email'],
+            "linkedin_url": application['linkedin_url'],
+            "education": application['education'],
+            "work_experience": application['work_experience'],
+            "resume_url": application['resume_url']
+        })
+        conn.commit()
